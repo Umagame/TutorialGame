58befe6dc98e479a1f9524846343a71db5c0e9cf
Umage <40861502+Umagame@users.noreply.github.com>
2024-01-08 17:28:16 +0900

    Add 8(2)

---
+++ 8/Player.cs
@@ -0,0 +1,191 @@
+//Player.cs
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float DashSpeed;
+    public float JumpForce;
+
+    public AudioSource JumpSE;
+    public AudioSource BreakSE;
+    public AudioSource MoneySE;
+
+    public LayerMask GroundLayer;
+
+    public GameObject MenuPanel;
+    public GameObject GameoverPanel;
+
+    public TextMeshProUGUI GameoverText;
+
+    public ParticleSystem BrickParticle1;
+    public ParticleSystem BrickParticle2;
+
+    private Rigidbody2D rb;
+
+    private bool isDashing = false;
+
+    public int Money;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        MoveSpeed = 3f;
+        DashSpeed = 6f;
+        JumpForce = 17f;
+        Money = 0;
+    }
+
+    void Update()
+    {
+        if(Time.timeScale != 0)
+        {
+            //Player Movement
+            if (!isDashing)
+            {
+                rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MoveSpeed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * DashSpeed, rb.velocity.y);
+            }
+
+            if (Input.GetButtonDown("Jump") && isGrounded())
+            {
+                rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+                JumpSE.Play();
+            }
+
+            if (Input.GetButton("Dash"))
+            {
+                isDashing = true;
+            }
+            else
+            {
+                isDashing = false;
+            }
+        }
+
+        if (Input.GetButtonDown("Escape"))
+        {
+            if (MenuPanel.activeSelf)
+            {
+                MenuPanel.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+            else
+            {
+                MenuPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+
+        //Sprite Flip
+        if (rb.velocity.x > 0)
+        {
+            GetComponent<Animator>().SetBool("flip", false);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            GetComponent<Animator>().SetBool("flip", true);
+        }
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        //Run
+        if (rb.velocity.x != 0)
+        {
+            if (!isDashing)
+            {
+                GetComponent<Animator>().SetInteger("state", 1);
+            }
+            else
+            {
+                GetComponent<Animator>().SetInteger("state", 4);
+            }
+        }
+        else
+        {
+            GetComponent<Animator>().SetInteger("state", 0);
+        }
+
+        //Jump / Fall
+        if (rb.velocity.y > 0.1f)
+        {
+            GetComponent<Animator>().SetInteger("state", 2);
+        }
+        else if (rb.velocity.y < -0.1f)
+        {
+            GetComponent<Animator>().SetInteger("state", 3);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        BoxCollider2D c = GetComponent<BoxCollider2D>();
+        return Physics2D.BoxCast(c.bounds.center, c.bounds.size, 0f, Vector2.down, .1f, GroundLayer);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject obj = collision.gameObject;
+
+        //InstaDeath
+        if (obj.CompareTag("InstaDeath"))
+        {
+            GameoverPanel.SetActive(true);
+            GameoverText.text = "Game Over!";
+            Time.timeScale = 0;
+        }
+
+        //Breakable
+        BoxCollider2D c = GetComponent<BoxCollider2D>();
+        if (obj.GetComponent<BreakableBricks>() != null && rb.velocity.y == 0 &&
+            obj.transform.position.y - obj.GetComponent<Collider2D>().bounds.size.y / 2 >= this.transform.position.y)
+        {
+            GameObject.Destroy(collision.gameObject);
+            BreakSE.Play();
+            if (obj.GetComponent<BreakableBricks>().ID == 1)
+            {
+                Instantiate(BrickParticle1, obj.transform.position, Quaternion.identity).GetComponent<ParticleSystem>().Play();
+            }
+            else if (obj.GetComponent<BreakableBricks>().ID == 2)
+            {
+                Instantiate(BrickParticle2, obj.transform.position, Quaternion.identity).GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject obj = collision.gameObject;
+
+        //Collectable
+        if (obj.GetComponent<Collectable>() != null)
+        {
+            if(obj.GetComponent<Collectable>().ID == "money")
+            {
+                Money++;
+                GameObject.Destroy(obj);
+                MoneySE.Play();
+            }
+        }
+
+        //Frag
+        if (obj.CompareTag("Frag"))
+        {
+            GameoverPanel.SetActive(true);
+            GameoverText.text = "Stage Completed!";
+            Time.timeScale = 0;
+        }
+    }
+}
